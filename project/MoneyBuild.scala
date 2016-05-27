d36b389bec3e36fea21166c8c12b268e504ce3db
Cleary, Paul <Paul_Cleary@cable.comcast.com>
2016-05-27 10:24:48 -0400

    Rolling 0.8.14

--- project/MoneyBuild.scala
+++ project/MoneyBuild.scala
@@ -224,7 +224,7 @@ object MoneyBuild extends Build {
 
   def basicSettings =  Defaults.itSettings ++ SbtScalariform.scalariformSettings ++ Seq(
     organization := "com.comcast.money",
-    version := "0.8.14-SNAPSHOT",
+    version := "0.8.14",
     crossScalaVersions := Seq("2.10.6", "2.11.7"),
     scalaVersion := "2.10.6",
     resolvers ++= Seq(
