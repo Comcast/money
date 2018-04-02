9b3167cc06ddc22fc4f88251d0357410d9d0f06e
ptravers <traverspw@gmail.com>
2018-04-02 17:42:04 -0400

    Refactor TracedFlow in to stream package to better delineate and follow akkas own pattern

---
+++ money-akka/src/main/scala/com/comcast/money/akka/stream/TracedFlow.scala
@@ -0,0 +1,52 @@
+package com.comcast.money.akka.stream
+
+import akka.actor.ActorSystem
+import akka.stream.stage.{GraphStage, GraphStageLogic}
+import akka.stream.{FlowShape, Inlet, Outlet, Shape}
+import com.comcast.money.akka.{MoneyExtension, SpanContextWithStack}
+import com.comcast.money.core.Tracer
+
+trait TracedFlow[In, Out] extends GraphStage[FlowShape[(In, SpanContextWithStack), (Out, SpanContextWithStack)]] {
+  implicit val actorSystem: ActorSystem
+
+  implicit lazy val moneyExtension: MoneyExtension = MoneyExtension(actorSystem)
+
+  val inletName: String
+  val outletName: String
+
+  type TracedIn = (In, SpanContextWithStack)
+  type TracedOut = (Out, SpanContextWithStack)
+
+  implicit val in: Inlet[TracedIn] = Inlet(name = inletName)
+  implicit val out: Outlet[TracedOut] = Outlet(name = outletName)
+
+  override def shape: FlowShape[TracedIn, TracedOut] = FlowShape.of(in, out)
+}
+
+abstract class TracedLogic[In, Out](shape: Shape)
+                                   (implicit moneyExtension: MoneyExtension) extends GraphStageLogic(shape) {
+  type TracedOut = (Out, SpanContextWithStack)
+  type TracedIn = (In, SpanContextWithStack)
+
+  implicit val in: Inlet[TracedIn] = shape.inlets.head.as[TracedIn]
+  implicit val out: Outlet[TracedOut] = shape.outlets.head.as[TracedOut]
+
+  private def tracer(implicit spanContext: SpanContextWithStack,
+                     moneyExtension: MoneyExtension): Tracer =
+    moneyExtension.tracer(spanContext)
+
+  def traceStageAndPush(key: String, stageLogic: In => Out): Unit = {
+    implicit val (inMessage, spanContext) = grab[TracedIn](in)
+    tracer.startSpan(key)
+    val outMessage = stageLogic(inMessage)
+    push[TracedOut](out, (outMessage, spanContext))
+  }
+
+  def traceStageAndStop(key: String, stageLogic: In => Out, isSuccessful: Boolean = true): Unit = {
+    implicit val (inMessage, spanContext): (In, SpanContextWithStack) = grab[TracedIn](in)
+    tracer.startSpan(key)
+    val outMessage = stageLogic(inMessage)
+    push[TracedOut](out, (outMessage, spanContext))
+    spanContext.getAll.foreach(_ => tracer.stopSpan(isSuccessful))
+  }
+}
