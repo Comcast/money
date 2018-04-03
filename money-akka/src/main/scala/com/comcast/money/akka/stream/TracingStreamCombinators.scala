ad4f576ffed193c310c2d2f9e18873d9d20b6e0b
ptravers <traverspw@gmail.com>
2018-04-03 15:29:03 -0400

    Add composer for finishing a flow and added generic naming

--- money-akka/src/main/scala/com/comcast/money/akka/stream/TracingStreamCombinators.scala
+++ money-akka/src/main/scala/com/comcast/money/akka/stream/TracingStreamCombinators.scala
@@ -7,6 +7,8 @@ import akka.stream.scaladsl.{Flow, Source, Unzip, Zip}
 import com.comcast.money.akka.{MoneyExtension, SpanContextWithStack}
 import com.comcast.money.core.Tracer
 
+import scala.reflect.{ClassTag, classTag}
+
 trait AkkaMoney {
   implicit val actorSystem: ActorSystem
 
@@ -22,56 +24,71 @@ trait TracingStreamCombinators {
 
   import akka.stream.scaladsl.GraphDSL.Implicits._
 
-  implicit class PortOpsSpanInserter[In, Out](portOps: PortOps[(In, SpanContextWithStack)])
-                                             (implicit builder: Builder[_]) {
+  implicit class PortOpsSpanInserter[In](portOps: PortOps[(In, SpanContextWithStack)])
+                                        (implicit builder: Builder[_], evidence: ClassTag[In]) {
 
-    def |~>(flow: Graph[FlowShape[In, Out], _]): PortOps[(Out, SpanContextWithStack)] = portOps ~> bounceSpanFlow(flow)
+    def |~>[Out: ClassTag](flow: Graph[FlowShape[In, Out], _]): PortOps[(Out, SpanContextWithStack)] = portOps ~> wrapWithSpanFlow(flow) ~> closeSpanFlow[Out]
 
-    def ~|>[T >: In](inlet: Inlet[T]): Unit = portOps ~> closingFlow ~> inlet
+    def ~|[T >: In](inlet: Inlet[T]): Unit = portOps ~> closeAllFlow[In] ~> inlet
 
-    private def closingFlow: Graph[FlowShape[(In, SpanContextWithStack), In], _] =
-      Flow.fromFunction[(In, SpanContextWithStack), In] {
+    def ~>|[Out: ClassTag](flow: Graph[FlowShape[In, Out], _]): PortOps[Out] = (portOps |~> flow) ~> closeAllFlow[Out]
+
+    private def closeAllFlow[T]: Graph[FlowShape[(T, SpanContextWithStack), T], _] =
+      Flow.fromFunction[(T, SpanContextWithStack), T] {
         case (input, spanContext) =>
           spanContext.getAll.foreach(_ => tracer(spanContext, moneyExtension).stopSpan())
           input
       }
+
+    private def closeSpanFlow[T]: Graph[FlowShape[(T, SpanContextWithStack), (T, SpanContextWithStack)], _] =
+      Flow.fromFunction[(T, SpanContextWithStack), (T, SpanContextWithStack)] {
+        case (input, spanContext) =>
+          tracer(spanContext, moneyExtension).stopSpan()
+          (input, spanContext)
+      }
   }
 
-  implicit class SourceSpanInserter[T](source: Source[T, _])(implicit builder: Builder[_]) {
+  implicit class SourceSpanInserter[T](source: Source[T, _])
+                                      (implicit builder: Builder[_],
+                                       evidence: ClassTag[T]) {
 
-    def |~>[Out](flow: Graph[FlowShape[T, Out], _]): PortOps[(Out, SpanContextWithStack)] = source ~> spanStart(source) |~> flow
+    private def spanStart[S: ClassTag](source: Source[S, _]): Flow[S, (S, SpanContextWithStack), _] =
+      Flow.fromFunction[S, (S, SpanContextWithStack)] {
+        input =>
+          val spanContext = new SpanContextWithStack
+          tracer(spanContext, moneyExtension).startSpan(s"SourceOf${nameOfType[S]}")
+          (input, spanContext)
+      }
+
+    def |~>[Out: ClassTag](flow: Graph[FlowShape[T, Out], _]): PortOps[(Out, SpanContextWithStack)] = source ~> spanStart[T](source) |~> flow
 
   }
 
-  private def bounceSpanFlow[In, Out](flow: Graph[FlowShape[In, Out], _])(implicit builder: Builder[_]) = {
+  private def wrapWithSpanFlow[In: ClassTag, Out: ClassTag](flow: Graph[FlowShape[In, Out], _])
+                                                           (implicit builder: Builder[_]) = {
+    val flowName = s"${nameOfType[In]}To${nameOfType[Out]}"
+
+    def spanFlow[T](flowName: String): Flow[(T, SpanContextWithStack), (T, SpanContextWithStack), _] =
+      Flow[(T, SpanContextWithStack)] map {
+        case (input, spanContext) =>
+          tracer(spanContext, moneyExtension).startSpan(flowName)
+          (input, spanContext)
+      }
+
     val unZip = builder.add(Unzip[In, SpanContextWithStack]())
     val zip = builder.add(Zip[Out, SpanContextWithStack]())
 
-    val spanFlowShape = builder.add(spanFlow[In](flow.toString))
+    val spanFlowShape = builder.add(spanFlow[In](flowName))
 
     val flowShape = builder.add(flow)
 
     spanFlowShape.out ~> unZip.in
 
     unZip.out0 ~> flowShape ~> zip.in0
-    unZip.out1     ~>     zip.in1
-
+    unZip.out1       ~>        zip.in1
 
     FlowShape(spanFlowShape.in, zip.out)
   }
 
-  private def spanFlow[T](flowName: String): Flow[(T, SpanContextWithStack), (T, SpanContextWithStack), _] =
-    Flow[(T, SpanContextWithStack)] map {
-      case (input, spanContext) =>
-        tracer(spanContext, moneyExtension).startSpan(flowName)
-        (input, spanContext)
-    }
-
-  private def spanStart[T](source: Source[T, _]): Flow[T, (T, SpanContextWithStack), _] =
-    Flow.fromFunction[T, (T, SpanContextWithStack)] {
-      input =>
-        val spanContext = new SpanContextWithStack
-        tracer(spanContext, moneyExtension).startSpan(s"${source.toString}")
-        (input, spanContext)
-    }
+  private def nameOfType[T: ClassTag]: String = classTag[T].runtimeClass.getSimpleName
 }
--- money-akka/src/test/scala/com/comcast/money/akka/stream/acceptance/MoneyStreamCombinatorsSpec.scala
+++ money-akka/src/test/scala/com/comcast/money/akka/stream/acceptance/MoneyStreamCombinatorsSpec.scala
@@ -1,8 +1,10 @@
 package com.comcast.money.akka.stream.acceptance
 
+import akka.Done
 import akka.actor.ActorSystem
-import akka.stream.ClosedShape
+import akka.stream.scaladsl.GraphDSL.Implicits.PortOps
 import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
+import akka.stream.{ClosedShape, SourceShape}
 import com.comcast.money.akka.Blocking.RichFuture
 import com.comcast.money.akka._
 import com.comcast.money.akka.stream.{AkkaMoney, TracingStreamCombinators}
@@ -12,30 +14,46 @@ import scala.concurrent.Future
 
 class MoneyStreamCombinatorsSpec extends MoneyAkkaScope {
 
-  "StreamCombinators" should {
-    "instrument a flow that does not take SpanContexts" in {
-      implicit val spanContextWithStack: SpanContextWithStack = new SpanContextWithStack
-
-      TestStream().run().get
+  "Tracing stream combinators" should {
+    "instrument a stream" in {
+      TestStream.simple().run().get
 
       val maybeHandler = MoneyExtension(system).handler.asInstanceOf[HandlerChain].handlers.headOption
       val maybeSpanHandler = maybeHandler.map(_.asInstanceOf[CollectingSpanHandler])
 
       maybeSpanHandler should haveSomeSpanNames(Seq("SourceOfString", "StringToString"))
     }
+
+    "instrument a source" in {
+      TestStream.sourceEndingWithFlow.runWith(Sink.ignore).get
+
+      val maybeHandler = MoneyExtension(system).handler.asInstanceOf[HandlerChain].handlers.headOption
+      val maybeSpanHandler = maybeHandler.map(_.asInstanceOf[CollectingSpanHandler])
+
+      maybeSpanHandler should haveSomeSpanNames(Seq("SourceOfString", "StringToString", "StringToString"))
+    }
   }
 
   object TestStream extends TracingStreamCombinators with AkkaMoney {
     override implicit val actorSystem: ActorSystem = _system
 
-    val sink = Sink.seq[String]
+    val sink = Sink.ignore
 
-    def apply() =
+    def simple() =
       RunnableGraph.fromGraph(GraphDSL.create(sink) {
-        implicit builder: GraphDSL.Builder[Future[Seq[String]]] => sink =>
-          (Source(List("chunk")) |~> Flow[String]) ~|> sink.in
+        implicit builder: GraphDSL.Builder[Future[Done]] => sink =>
+
+          (Source(List("chunk")) |~> Flow[String]) ~| sink.in
 
           ClosedShape
+      })
+
+    def sourceEndingWithFlow =
+      Source.fromGraph(GraphDSL.create() {
+        implicit builder =>
+          val out: PortOps[String] = (Source(List("chunk")) |~> Flow[String]) ~>| Flow[String]
+
+          SourceShape(out.outlet)
       })
   }
 }
