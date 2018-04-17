102e09ef576c4185426fd8a89ff44da8a3d1a7f8
Knowles, Mark <Mark_Knowles@cable.comcast.com>
2018-04-17 15:08:53 -0700

    Initial commit for ElkSpanHandler

---
+++ money-core/src/main/scala/com/comcast/money/core/handlers/ElkSpanHandler.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012-2015 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.comcast.money.core.handlers
+
+import com.comcast.money.api.{ Note, SpanInfo }
+import com.typesafe.config.Config
+import org.slf4j.{ LoggerFactory, MDC }
+
+class ElkSpanHandler extends ConfigurableHandler {
+  import com.comcast.money.core.handlers.LoggingSpanHandler._
+
+  private val logger = LoggerFactory.getLogger("money-elk")
+  protected var logFunction: LogFunction = logger.info
+
+  def configure(config: Config): Unit = {
+
+    if (config.hasPath("log-level")) {
+      val level = config.getString("log-level").toUpperCase
+
+      // set the log level based on the configured value
+      level match {
+        case "ERROR" => logFunction = logger.error
+        case "WARN" => logFunction = logger.warn
+        case "INFO" => logFunction = logger.info
+        case "DEBUG" => logFunction = logger.debug
+        case "TRACE" => logFunction = logger.trace
+      }
+    }
+  }
+
+  def handle(spanInfo: SpanInfo): Unit = {
+    import scala.collection.JavaConverters._
+    val baseFields = Seq(
+      // The field names below are the same as cedi-dtrace. This makes it easier to query a transaction in elastic search.
+      ("trace-id", spanInfo.id.traceId()),
+      ("parent-id", spanInfo.id.parentId()),
+      ("span-id", spanInfo.id.selfId()),
+      ("span-name", spanInfo.name()),
+      ("app", spanInfo.appName()),
+      ("host", spanInfo.host()),
+      ("start-time", spanInfo.startTimeMillis()),
+      ("end-time", spanInfo.endTimeMillis()),
+      ("span-duration", spanInfo.durationMicros()),
+      ("span-success", spanInfo.success())
+    )
+    val noteFields: Seq[(String, Any)] = spanInfo.notes.values.asScala.map(n => (n.name(), n.value())).toSeq
+    val allFields = baseFields ++ noteFields
+
+    allFields.foreach(p => MDC.put(p._1, p._2.toString))
+
+    logFunction(allFields.map { case (k, v) => s"$k:$v" }.mkString("[", "][", "]"))
+  }
+}
