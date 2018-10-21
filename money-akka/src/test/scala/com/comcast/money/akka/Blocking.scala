9b36eca3d92ff086c1b7325427630341c27d7ab2
ptravers <traverspw@gmail.com>
2018-10-21 10:08:33 -0400

    Refactor max timeout from 5 seconds to 60 seconds

--- money-akka/src/test/scala/com/comcast/money/akka/Blocking.scala
+++ money-akka/src/test/scala/com/comcast/money/akka/Blocking.scala
@@ -22,7 +22,7 @@ import scala.concurrent.{ Await, Future }
 object Blocking {
 
   implicit class RichFuture[T](future: Future[T]) {
-    def get(duration: FiniteDuration = 5 seconds): T = Await.result(future, duration)
+    def get(duration: FiniteDuration = 60 seconds): T = Await.result(future, duration)
   }
 
 }
