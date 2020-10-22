bfdca2f7f053f1b66487e2bc2612202d2c41ef02
Spindler, Justin <Justin_Spindler@comcast.com>
2020-10-22 13:51:51 -0400

    Formatting+headers

--- money-core/src/test/scala/com/comcast/money/core/formatters/FormatterPropagatorSpec.scala
+++ money-core/src/test/scala/com/comcast/money/core/formatters/FormatterPropagatorSpec.scala
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2012 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.comcast.money.core.formatters
 
 import com.comcast.money.api.SpanId
 import io.grpc.Context
 import io.opentelemetry.context.propagation.TextMapPropagator
-import io.opentelemetry.trace.{DefaultSpan, TraceFlags, TraceState, TracingContextUtils}
+import io.opentelemetry.trace.{ DefaultSpan, TraceFlags, TraceState, TracingContextUtils }
 import org.mockito.ArgumentCaptor
 import org.mockito.Matchers.any
-import org.mockito.Mockito.{verify, when}
+import org.mockito.Mockito.{ verify, when }
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
--- money-core/src/test/scala/com/comcast/money/core/spi/MoneyTracerProviderFactorySpec.scala
+++ money-core/src/test/scala/com/comcast/money/core/spi/MoneyTracerProviderFactorySpec.scala
@@ -1,15 +1,34 @@
+/*
+ * Copyright 2012 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.comcast.money.core.spi
 
 import java.util.ServiceLoader
 
-import com.comcast.money.core.Tracer
+import com.comcast.money.core.{ Money, Tracer }
+import io.opentelemetry.OpenTelemetry
 import io.opentelemetry.trace.spi.TracerProviderFactory
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
+
 import scala.collection.JavaConverters._
 
 class MoneyTracerProviderFactorySpec extends AnyWordSpec with MockitoSugar with Matchers {
+
   "MoneyTracerProviderFactory" should {
     "wrap an existing tracer in a TracerProvider" in {
       val tracer = mock[Tracer]
@@ -27,6 +46,11 @@ class MoneyTracerProviderFactorySpec extends AnyWordSpec with MockitoSugar with 
       result.isDefined shouldBe true
       val factory = result.get
       factory shouldBe a[MoneyTracerProviderFactory]
+    }
+
+    "integrates with OpenTelemetry.getTracer" in {
+      val tracer = OpenTelemetry.getTracer("test")
+      tracer shouldBe Money.Environment.tracer
     }
   }
 }
