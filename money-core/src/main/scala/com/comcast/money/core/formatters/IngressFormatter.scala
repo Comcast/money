d0f26def2e697ccf1c12e62e948cfd4be105b5b5
Spindler, Justin <Justin_Spindler@comcast.com>
2020-10-27 13:46:54 -0400

    Finally

--- money-core/src/main/scala/com/comcast/money/core/formatters/IngressFormatter.scala
+++ money-core/src/main/scala/com/comcast/money/core/formatters/IngressFormatter.scala
@@ -20,7 +20,7 @@ import com.comcast.money.api.SpanId
 /**
  * Decorates a formatter so that it interprets headers for incoming requests but does not write headers for outgoing requests.
  */
-class IngressFormatter(val formatter: Formatter) extends Formatter {
+final class IngressFormatter(val formatter: Formatter) extends Formatter {
   override def toHttpHeaders(spanId: SpanId, addHeader: (String, String) => Unit): Unit = ()
   override def fromHttpHeaders(getHeader: String => String, log: String => Unit): Option[SpanId] =
     formatter.fromHttpHeaders(getHeader, log)
